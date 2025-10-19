4dff4dbdd3d3a8a37c817a9bc45fa6a631ecdaf1
lsilvpin <lsilvpin@gmail.com>
2025-10-19 10:12:46 -0300

    Projeto configurado com relatório de cobertura

--- app/jest.config.js
+++ app/jest.config.js
@@ -7,4 +7,10 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/index.js',
+    '!src/setupTests.js',
+  ],
+  coverageReporters: ['html', 'text'],
 };
