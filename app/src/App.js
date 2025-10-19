db964c89c9ca7c627915cd7376e159fd8e72fb56
lsilvpin <lsilvpin@gmail.com>
2025-10-18 23:23:46 -0300

    Ajuste para botão de mover carrossel fique no lugar certo

--- app/src/App.js
+++ app/src/App.js
@@ -127,9 +127,9 @@ function App() {
         <section className="instructor">
           <h3>Sobre o Instrutor</h3>
           <div className="instructor-card">
-            <img src={`${process.env.PUBLIC_URL}/images/instructor.jpg`} alt="Instrutor Alex Silva" />
+            <img src={`${process.env.PUBLIC_URL}/images/instructor.jpg`} alt="Instrutor EuCoder123" />
             <div className="instructor-info">
-              <h4>Alex Silva</h4>
+              <h4>EuCoder123</h4>
               <p className="instructor-title">Desenvolvedor de Jogos & Especialista em Go</p>
               <p>Com 5 anos de experiência, já criei jogos publicados na itch.io e Steam. Apaixonado por ensinar Go e Ebiten de forma prática.</p>
               <div className="instructor-stats">
